a50b68b2b17f4f44f114fb03480682b8f0873e70
Vu Anh Hao <hi@haova.me>
2022-07-29 13:46:36 +0700

    Server as a service

--- src/index.js
+++ src/index.js
@@ -1,36 +1,21 @@
 import Koa from 'koa';
+import colors from 'colors';
 import koaBody from 'koa-body';
 import cors from '@koa/cors';
 import applyQueryString from 'koa-qs';
-import colors from 'colors';
+import Router from '@koa/router';
+
+import { curry } from 'ramda';
 import { FileData } from '@rugo-vn/common';
 
-/**
- *
- * @typedef {object} Instance
- * @property {object} koa Origin koa object.
- * @property {object} context Alias context of koa.
- * @property {object} listener HTTP Listener.
- * @property {Function} close Async. Close server instance.
- * @property {Function} listen Async. Start the server.
- * @property {Function} address Get address of server.
- */
+export const name = 'server';
 
-/**
- * Logging request information.
- *
- * @param {object} ctx Current context.
- * @param {Function} next next function in koa.
- */
-const logMiddleware = async (ctx, next) => {
+const log = async (logger, ctx, next) => {
   const ltime = new Date();
   await next();
   const ctime = new Date();
 
-  console.log(
-    colors.yellow('[server] ') +
-    colors.grey(ctime.toISOString()) +
-    ' ' +
+  logger.info(
     colors.yellow(ctx.method) +
     ' ' +
     (Math.floor(ctx.status / 100) === 2 ? colors.green(ctx.status) : colors.red(ctx.status)) +
@@ -41,19 +26,47 @@ const logMiddleware = async (ctx, next) => {
   );
 };
 
+const createRouteHandle = async (service, address, ctx, next) => {
+  const res = await service.call(address, {
+    params: ctx.params,
+    form: ctx.form,
+    headers: ctx.headers
+  });
+
+  let done = true;
+
+  if (!res) { done = false; }
+
+  if (res && !res.status && !res.body) { done = false; }
+
+  if (!done) { return await next(); }
+
+  const headers = res.headers || [];
+  for (const [key, value] of headers) {
+    ctx.set(key, value);
+  }
+
+  ctx.status = res.status || 200;
+  ctx.body = res.body || '';
+};
+
+export const actions = {
+  address () {
+    return `http://127.0.0.1:${this.settings.port}`;
+  }
+};
+
 /**
- * Create server instance
  *
- * @param {number} port Port to mount.
- * @returns {Instance} Return instance of server.
  */
-const createServer = (port) => {
+export async function started () {
   // create server
   const server = new Koa();
 
   // middlewares
-  server.use(logMiddleware);
+  server.use(curry(log)(this.logger));
 
+  // parser
   applyQueryString(server);
 
   server.use(cors());
@@ -65,41 +78,39 @@ const createServer = (port) => {
       ...ctx.request.files
     };
 
-    for (let key in ctx.form){
-      if (ctx.form[key] && ctx.form[key].constructor.name === 'PersistentFile'){
-        ctx.form[key] = new FileData(ctx.form[key].filepath)
+    for (const key in ctx.form) {
+      if (ctx.form[key] && ctx.form[key].constructor.name === 'PersistentFile') {
+        ctx.form[key] = new FileData(ctx.form[key].filepath);
       }
     }
 
     return await next();
   });
 
-  return {
-    koa: server,
-    context: server.context,
+  // routing
+  const router = new Router();
+  const routeHandle = curry(createRouteHandle)(this);
 
-    async close () {
-      if (!this.listener) { return; }
+  for (const route of this.settings.routes || []) {
+    router[route.method.toLowerCase()](route.path, routeHandle(route.address));
+  }
 
-      await this.listener.close();
-      this.listener = null;
-    },
+  server.use(router.routes());
 
-    use (...args) {
-      server.use(...args);
-    },
+  // listen
+  await new Promise(resolve => {
+    this.listener = server.listen(this.settings.port, () => {
+      resolve();
+    });
+  });
 
-    async listen () {
-      await new Promise(resolve => {
-        this.listener = server.listen(port, () => {
-          resolve(this);
-        });
-      });
-    },
+  this.logger.info(`Server is running at port ${this.settings.port}`);
+}
 
-    address () {
-      return `http://127.0.0.1:${port}`;
-    }
-  };
-};
-export default createServer;
+/**
+ *
+ */
+export async function stopped () {
+  await this.listener.close();
+  delete this.listener;
+}
--- test/index.test.js
+++ test/index.test.js
@@ -2,45 +2,91 @@
 
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import log4js from 'log4js';
+import { mergeDeepLeft, mergeDeepRight } from 'ramda';
 
-import createServer from "../src/index.js";
+const logger = log4js.getLogger();
+logger.level = "info";
+
+import * as originServer from '../src/index.js';
 
 chai.use(chaiHttp);
 
+const createServer = (config = {}) => mergeDeepRight({
+  ...originServer,
+  settings: { port: 3000 },
+  logger: logger,
+  call(name, ...params){
+    switch (name) {
+      case 'demo.home':
+        return { status: 200, body: 'ok', headers: [['Content-Type', 'text/plain']] };
+
+      case 'demo.notfound':
+        return null;
+
+      case 'demo.custom':
+        return { status: 400, body: 'Something wrong' };
+
+      default:
+        return this.actions[name].bind(this)(...params);
+    }
+  }
+}, config);
+
 describe("Server test", () => {
   it('should create server', async () => {
-    const server = await createServer(3000);
+    const server = createServer();
 
-    expect(server).to.has.property('koa');
-    expect(server).to.has.property('context');
-    expect(server).to.has.property('listen');
-    expect(server).to.has.property('close');
-    expect(server).to.has.property('address');
-    expect(server).to.not.has.property('listener');
+    await server.started();
 
-    await server.listen()
+    // get address
+    expect(server.call('address')).to.be.eq('http://127.0.0.1:3000');
 
-    expect(server).to.has.property('listener');
-    expect(server.address()).to.be.eq('http://127.0.0.1:3000');
+    // simple get
+    const res = await chai.request(server.call('address'))
+      .get('/');
 
-    server.use((ctx) => { ctx.body = 'ok'; if (ctx.query.error){ ctx.status = 404 }; });
+    expect(res).to.has.property('status', 404);
+    expect(res.text).to.be.eq('Not Found');
 
-    const res = await chai.request(server.address())
-    .get('/');
+    await server.stopped();
+  });
+
+  it('should routing', async () => {
+    const server = createServer({
+      settings: {
+        routes: [
+          { method: 'get', path: '/', address: 'demo.home' },
+          { method: 'get', path: '/not-found', address: 'demo.notfound' },
+          { method: 'get', path: '/custom', address: 'demo.custom'}
+        ]
+      }
+    });
+
+    await server.started();
+
+    // 200
+    const res = await chai.request(server.call('address'))
+      .get('/');
 
     expect(res).to.has.property('status', 200);
     expect(res.text).to.be.eq('ok');
+    expect(res.headers).to.has.property('content-type', 'text/plain');
 
-    const res2 = await chai.request(server.address()).get('/error/request?error=true');
+    // 404
+    const res2 = await chai.request(server.call('address'))
+      .get('/not-found');
+
     expect(res2).to.has.property('status', 404);
-    expect(res2.text).to.be.eq('ok');
+    expect(res2.text).to.be.eq('Not Found');
 
-    await chai.request(server.address()).get('/this/is/a/long/request');
-    await chai.request(server.address()).get('/this/is/a/long/request/2');
+    // custom
+    const res3 = await chai.request(server.call('address'))
+      .get('/custom');
 
-    await server.close();
-    expect(server).to.has.property('listener', null);
+    expect(res3).to.has.property('status', 400);
+    expect(res3.text).to.be.eq('Something wrong');
 
-    await server.close();
+    await server.stopped();
   });
 });
